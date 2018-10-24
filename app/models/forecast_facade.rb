8a83df26743f57390dc5a3d12eb7d5a024df66a0
Kathleen Yruegas <kathleenecn@gmail.com>
2018-10-24 15:08:11 -0600

    Adds api call method to facade

--- app/models/forecast_facade.rb
+++ app/models/forecast_facade.rb
@@ -1,5 +1,21 @@
 class ForecastFacade
   def initialize(zip_code)
     @zip_code = zip_code
-  end 
+  end
+
+  def zip_code
+    @zip_code
+  end
+
+  def location
+    @zip_code.to_lat + "," + @zip_code.to_lon
+  end
+
+
+
+  def dark_sky_data
+    response = Faraday.get("https://api.darksky.net/#{ENV["darksky_api_key"]}/#{location}")
+    raw_forecast_data = JSON.parse(response.body, symbolize_names: true)[]
+
+  end
 end
--- spec/facades/forecast_facade_spec.rb
+++ spec/facades/forecast_facade_spec.rb
@@ -1,8 +1,33 @@
 require "rails_helper"
 
 describe ForecastFacade do
-  subject { ForecastFacade.new(zip_code: "80203")}
+
+  subject { ForecastFacade.new("80203")}
+
   it 'exists' do
     expect(subject).to be_a(ForecastFacade)
+  end
+
+  it 'has zip code attribute' do
+    expect(subject.zip_code).to eq("80203")
+  end
+
+  context 'instance methods' do
+    context '#dark_sky_data' do
+      it 'returns dark sky json data' do
+        results = subject.dark_sky_data
+
+        expect(results.first).to have_key(:time)
+        expect(results.first).to have_key(:moonPhase)
+        expect(results.first).to have_key(:precipProbability)
+        expect(results.first).to have_key(:cloudCover)
+      end
+    end
+
+    context "#location" do
+      it 'returns string of lat and long' do
+        expect(subject.location).to eq("39.731286,-104.98306")
+      end
+    end
   end
 end
