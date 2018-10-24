180dc378227cd6c259bb456b3d24fb529416aa8e
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-10-24 13:34:37 -0600

    draft spec for landing page feature

---
+++ spec/features/user_visits_root_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe 'user visits the landing page' do
+  context 'as a visitor' do
+    visit '/'
+
+    expect(page).to have_link "Log In / Sign Up"
+    expect(page).to have_button "Find a Party"
+    expect(page).to have_button "Plan a Party"
+    expect(page).to have_css(".apod")
+
+  end
+end
