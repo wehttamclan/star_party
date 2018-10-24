f1ee7d21f37ca054316f1199ec46f022e4ff6f61
Kathleen Yruegas <kathleenecn@gmail.com>
2018-10-24 13:56:23 -0600

    Adds feature test for forecast page

---
+++ spec/features/user_can_see_10_day_forecast_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+feature "As an authenticated user" do
+  scenario "I can view ten day forecast with valid zip code" do
+    # work in authentication once that's built in
+    # visit '/'
+    #
+    # fill_in :zip_code, with: "80203"
+    # expect(current_page).to eq('/forecast')
+    visit '/forecast', params: {zip_code: "80203"} #delete this line once root page and button exist
+
+    expect(page).to have_css('.day', count: 10)
+
+    within(first('.day')) do
+      expect(page).to have_css('.date')
+      expect(page).to have_css('.cloud_coverage')
+      expect(page).to have_css('.moon_phase')
+      expect(page).to have_css('.rain_chance')
+      expect(page).to have_css('.rating')
+    end
+  end
+end
