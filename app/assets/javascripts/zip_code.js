8aa0461ce498f8dd650245b051ff53f1181e074c
bghalami <benjaminghalami@gmail.com>
2018-10-25 02:09:52 -0600

    Almost have the toggling finished

--- app/assets/javascripts/zip_code.js
+++ app/assets/javascripts/zip_code.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
+  $(document).click(function(){
+    $('.hidden-form').slideUp();
+  });
+  $('.zip-search').click(function(button) {
+    button.stopPropagation();
+  });
+
   $('.hidden-form').hide();
   $('.hidden-button').hide();
   $(".zip-search").click(function(){
+    if ($('.hidden-drop').height() > 0) {
+      $('.hidden-form').slideUp();
+    }
     $(this).parent().children('.hidden-form').slideToggle(function(){
-      $(this).children(".hidden-drop").focus()
+      $(this).children(".hidden-drop").focus();
     });
   });
 });
