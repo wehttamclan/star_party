5d65a8a428054c8046cf4ed278f477010e454e4f
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-10-25 18:01:26 -0600

    update view with parsed date

--- app/models/party.rb
+++ app/models/party.rb
@@ -3,6 +3,11 @@ class Party < ApplicationRecord
                         :date,
                         :zip_code
 
+
+  def view_date
+    self.date.strftime('%b. %-d, %Y')
+  end
+
   def location
     "#{self.street_address}, #{self.city}, #{self.state} #{self.zip_code}"
   end
--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -14,5 +14,7 @@ describe Party, type: :model do
       expect(p.location).to eq(
         "#{p.street_address}, #{p.city}, #{p.state} #{p.zip_code}")
     end
+
+
   end
 end
