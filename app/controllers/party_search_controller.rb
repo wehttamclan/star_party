67ededbf1323bdbcfaa93e1d3af3948dca0beedc
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-10-25 19:08:44 -0600

    test passing for flash and redirect on zero party results, but flash not displaying on root

--- app/controllers/party_search_controller.rb
+++ app/controllers/party_search_controller.rb
@@ -2,7 +2,7 @@ class PartySearchController < ApplicationController
   def index
     @party_search_result = PartySearchResult.new(params[:q_find])
     if @party_search_result.parties == []
-      flash[:notice] = "No parties found in zipcode #{params[:zip_code]}. Try another search!"
+      flash[:notice] = "No parties found. Try another search!"
       redirect_to "/"
     end
   end
