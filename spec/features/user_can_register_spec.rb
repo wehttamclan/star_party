013ddeb5f7b0908b3d8036cfc969a22d7a43d543
bghalami <benjaminghalami@gmail.com>
2018-10-28 18:34:51 -0600

    made user register test

---
+++ spec/features/user_can_register_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+describe "As a visitor" do
+  describe "I can create and account" do
+    it "should let me register and redirect me to the dashboard" do
+      user_info = { name: "Bob Job",
+                    email: "bob@bob.com",
+                    phone_number: "123-456-7890",
+                    zip_code: 80202
+                  }
+
+      visit "/"
+
+      click_on("Log In / Sign Up")
+      expect(current_path).to eq("/login")
+
+      click_on("Register Here")
+      expect(current_path).to eq("/users/new")
+
+      fill_in "Name", with: user_info[:name]
+      fill_in "Email", with: user_info[:email]
+      fill_in "Phone Number", with: user_info[:phone_number]
+      fill_in "Zip Code", with: user_info[:zip_code]
+
+      click_on("Create User")
+
+      expect(current_path).to eq("/dashboard")
+
+      expect(page).to have_content("Welcome, #{user_info[:name]}")
+
+      expect(page).to have_link("My Account")
+      expect(page).to_not have_link("Log In / Sign Up")
+    end
+  end
+end
--- spec/features/user_can_see_10_day_forecast_spec.rb
+++ spec/features/user_can_see_10_day_forecast_spec.rb
@@ -4,7 +4,7 @@ feature "As an authenticated user" do
   scenario "I can view ten day forecast with valid zip code" do
     # work in authentication once that's built in
     # visit '/'
-    
+
     # fill_in :zip_code, with: "80203"
     # expect(current_page).to eq('/forecast')
     visit forecast_path(zip_code: "80203") #delete this line once root page and button exist
