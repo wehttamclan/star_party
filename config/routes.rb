6e5651f81b33782ef5c117f0aed7371140923586
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-10-29 17:26:17 -0600

    add api routes and update draft spec, regenerate gemfile.lock

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,22 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 
+  namespace :api do
+    namespace :v1 do
+      namespace :parties do
+        get '/find_all', to: 'search#index'
+        get '/find', to: 'search#show'
+      end
+    end
+  end
+
   get '/forecast', to: 'forecast#index'
 
   get "/party_search", to: 'party_search#index'
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  
+
   get '/logout', to: 'sessions#destroy'
 
   get '/dashboard', to: 'dashboard#show'
--- spec/requests/api/v1/parties_request_spec.rb
+++ spec/requests/api/v1/parties_request_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 describe "Parties API" do
+  it "sends a list of merchants" do
+    create_list(:party, 3)
 
+    get '/api/v1/parties'
+
+    expect(response).to be_successful
+
+    parties = JSON.parse(response.body)
+
+    expect(parties).count to eq(3)
+  end
 end
