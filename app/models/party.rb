85afac479bf464836be966b4da7f1b4cfc632f54
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-10-28 22:01:05 -0600

    adds caching

--- app/models/party.rb
+++ app/models/party.rb
@@ -3,6 +3,16 @@ class Party < ApplicationRecord
                         :date,
                         :zip_code
 
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
+
+  def address
+    [street_address, city, zip, state].compact.join(", ")
+  end
+
+  def address_changed?
+    street_address_changed? || city_changed? || zip_changed? || state_changed?
+  end
 
   def view_date
     self.date.strftime('%b. %-d, %Y')
@@ -11,5 +21,4 @@ class Party < ApplicationRecord
   def location
     "#{self.street_address}, #{self.city}, #{self.state} #{self.zip_code}"
   end
-
 end
