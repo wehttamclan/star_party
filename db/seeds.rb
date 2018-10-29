edad78b3716583bae9d664a6cf7dbcb3c4a09587
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-10-28 22:04:16 -0600

    gives seeds real addresses (local parks) so geocoding will function

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,23 +10,23 @@ Party.destroy_all
 Party.create(title: "Comet Party",
           date: "01-01-2019",
           description: "View Halley's Comet",
-          street_address: "100 Host Place",
-          city: "Starville",
+          street_address: "9357 W 26th Ave",
+          city: "Wheat Ridge",
           state: "CO",
-          zip_code: 80203)
+          zip_code: 80033)
 
 Party.create(title: "Nebula Party",
           date: "11-11-2018",
           description: "View Turing Nebula",
-          street_address: "888 Galaxy Road",
-          city: "Starville",
+          street_address: "1700 N Sheridan Blvd",
+          city: "Denver",
           state: "CO",
-          zip_code: 80203)
+          zip_code: 80212)
 
 Party.create(title: "Meteor Party",
           date: "07-07-2017",
           description: "View Lovelace Meteor Shower",
-          street_address: "42 Universe Way",
-          city: "Douglastown",
+          street_address: "15600 W Morrison Rd",
+          city: "Lakewood",
           state: "CO",
-          zip_code: 80203)
+          zip_code: 80228)
