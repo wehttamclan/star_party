86a05669c1196e3bb2f2f265e8f43c4d4fba419a
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-10-29 17:05:39 -0600

    begin draft of party API request spec, update party factory

--- spec/factories/parties.rb
+++ spec/factories/parties.rb
@@ -2,9 +2,9 @@ FactoryBot.define do
   factory :party do
     title { "Sample Title" }
     date { "01-01-2019" }
-    description { "Viewing the Turing Nebula" }
-    street_address { "5000 Miles Road" }
-    city { "Starville" }
+    description { "Viewing the Sample Celestial Event" }
+    street_address { "1000 Sample Road" }
+    city { "Sampleville" }
     state { "CO"}
     zip_code { 80203 }
   end
---
+++ spec/requests/api/v1/parties_request_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+describe "Parties API" do
+
+end
