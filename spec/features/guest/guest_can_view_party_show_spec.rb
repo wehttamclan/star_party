47d5ffbaa8dc92ac2573df7f32d8ca2e5e0bb98e
Kathleen Yruegas <kathleenecn@gmail.com>
2018-10-29 20:51:35 -0600

    Adds test for guest to not see party contents

---
+++ spec/features/guest/guest_can_view_party_show_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+feature 'As an guest user' do
+  context 'I can visit a party show' do
+    scenario 'and view party details' do
+      fun_party = create(:party)
+
+      visit "/parties/#{fun_party.id}"
+
+      expect(page).to have_content(fun_party.title)
+      expect(page).to have_content("Date: #{fun_party.date.strftime('%A, %b %d')}")
+      expect(page).to have_content(fun_party.description)
+      expect(page).to have_content("Please log in to view party details and to Attend.")
+      expect(page).to_not have_content("Hosted By: #{fun_party.host.name}")
+      expect(page).to_not have_content("Attend")
+    end
+  end
+end
