33a00121d15024a67a5fee3015b4aa3681908759
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-10-29 18:37:05 -0600

    adds functional API request endpoints for all parties, party by id, and all parties matching zip code

--- config/routes.rb
+++ config/routes.rb
@@ -4,26 +4,33 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      
       namespace :parties do
-        get '/find_all', to: 'search#index'
-        get '/find', to: 'search#show'
+        get '/find_all', to: 'search#index' #by zip
       end
+
+      scope module: 'parties' do
+        resources :parties, only: [:index, :show]
+      end
+
     end
   end
+
+
 
   get '/forecast', to: 'forecast#index'
 
   get "/party_search", to: 'party_search#index'
 
+  resources :parties, only: [:new, :create, :show]
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  get '/auth/:provider/callback', to: 'sessions#create'
 
   get '/logout', to: 'sessions#destroy'
 
   get '/dashboard', to: 'dashboard#show'
 
   resources :users, only: [:new, :create]
-
-  resources :parties, only: [:new, :create]
 
 end
--- spec/requests/api/v1/parties_request_spec.rb
+++ spec/requests/api/v1/parties_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe "Parties API" do
-  it "sends a list of merchants" do
+  it "sends a list of parties" do
     create_list(:party, 3)
 
     get '/api/v1/parties'
@@ -10,6 +10,32 @@ describe "Parties API" do
 
     parties = JSON.parse(response.body)
 
-    expect(parties).count to eq(3)
+    expect(parties.count).to eq(3)
+  end
+
+  it "can get one party by its id" do
+    id = create(:party).id
+
+    get "/api/v1/parties/#{id}"
+
+    party = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(party["id"]).to eq(id)
+  end
+
+  it "returns all parties by zip code" do
+    party1, party2 = create_list(:party, 2, zip_code: 80000)
+    party3 = create(:party, zip_code: 70000)
+
+    get "/api/v1/parties/find_all?zip_code=#{party1.zip_code}"
+
+    expect(response).to be_successful
+
+    parties = JSON.parse(response.body)
+
+    expect(response.body).to include("#{party1.id}")
+    expect(response.body).to include("#{party2.id}")
+    expect(response.body).to_not include("#{party3.id}")
   end
 end
