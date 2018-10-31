0eb540bd355816e6c401325c47963843331c2eee
bghalami <benjaminghalami@gmail.com>
2018-10-31 14:26:04 -0600

    making oauth test

--- spec/features/user_can_log_in_test_spec.rb
+++ spec/features/user_can_log_in_test_spec.rb
@@ -19,4 +19,27 @@ describe "As a guest user" do
       expect(current_path).to eq("/dashboard")
     end
   end
+  describe "When I log in with google" do
+    it "should take me to my dashboard" do
+      def stub_omniauth
+        OmniAuth.config.test_mode = true
+        OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new({
+          info: { email: "hand@hand.com", name: "Ben"},
+          :uid => "11"
+        })
+      end
+      
+      stub_omniauth
+
+      VCR.use_cassette("visit login page") do
+        visit("/login")
+      end
+
+      VCR.use_cassette("click login with google") do
+        click_on("Login with Google")
+      end
+
+      expect(current_path).to eq("/dashboard")
+    end
+  end
 end
