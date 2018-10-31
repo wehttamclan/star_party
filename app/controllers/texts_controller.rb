a0abb587152a47c59677f3b62f4c39dfda953d23
Kathleen Yruegas <kathleenecn@gmail.com>
2018-10-31 08:35:05 -0600

    Cleans up comments in controller

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -9,13 +9,6 @@ class TextsController < ApplicationController
     twilio = TwilioFacade.new(friend_name, current_user.name, params[:phone_number], party_id)
     twilio.text
 
-    # client = Twilio::REST::Client.new(ENV["twilio_account_sid"], ENV["twilio_auth_token"])
-    # client.messages.create(
-    #   from: ENV["twilio_phone_number"],
-    #   to: friend_phone,
-    #   body: "Hi #{friend_name}! #{current_user.name} is attending a
-    #   Star Party and wants you to join! You can see more info at #{link}.")
-
     flash[:alert] = "Your text message to #{friend_name} was sent!"
     redirect_to '/dashboard'
   end
