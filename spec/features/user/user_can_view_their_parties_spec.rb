d8e6852cc30966fd79aa77726e44daf73e136a11
Kathleen Yruegas <kathleenecn@gmail.com>
2018-11-01 13:08:30 -0600

    Fixes VCR on test

--- spec/features/user/user_can_view_their_parties_spec.rb
+++ spec/features/user/user_can_view_their_parties_spec.rb
@@ -7,35 +7,37 @@ feature "As an authenticated user" do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
       VCR.use_cassette("create_6_parties") do
-        party_1 = create(:party, host_id: user.id)
-        party_2 = create(:party)
-        party_3 = create(:party)
-        party_4 = create(:party)
-        party_5 = create(:party, host_id: user.id)
-        party_6 = create(:party)
+        @party_1 = create(:party, host_id: user.id)
+        @party_2 = create(:party)
+        @party_3 = create(:party)
+        @party_4 = create(:party)
+        @party_5 = create(:party, host_id: user.id)
+        @party_6 = create(:party)
 
-        party_2.users << user
-        party_6.users << user
+        @party_2.users << user
+        @party_6.users << user
+      end
 
+      VCR.use_cassette('new-dashboard') do
         visit '/dashboard'
+      end
 
         within('.hosted') do
-          expect(page).to have_content(party_1.title)
-          expect(page).to have_content(party_1.date)
-          expect(page).to have_content(party_5.title)
-          expect(page).to have_content(party_5.date)
+          expect(page).to have_content(@party_1.title)
+          expect(page).to have_content(@party_1.date)
+          expect(page).to have_content(@party_5.title)
+          expect(page).to have_content(@party_5.date)
         end
 
         within('.attending') do
-          expect(page).to have_content(party_2.title)
-          expect(page).to have_content(party_2.date)
-          expect(page).to have_content(party_6.title)
-          expect(page).to have_content(party_6.date)
+          expect(page).to have_content(@party_2.title)
+          expect(page).to have_content(@party_2.date)
+          expect(page).to have_content(@party_6.title)
+          expect(page).to have_content(@party_6.date)
         end
 
-        expect(page).to_not have_content(party_3.title)
-        expect(page).to_not have_content(party_4.title)
-      end
+        expect(page).to_not have_content(@party_3.title)
+        expect(page).to_not have_content(@party_4.title)
     end
   end
 end
