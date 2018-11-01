67899eeb85e8644c71faadc77d82d0ed3cffa9c1
Kathleen Yruegas <kathleenecn@gmail.com>
2018-11-01 12:31:13 -0600

    Starts features test for user dashboard

--- spec/features/user/user_can_view_their_parties_spec.rb
+++ spec/features/user/user_can_view_their_parties_spec.rb
@@ -6,12 +6,26 @@ feature "As an authenticated user" do
       user = create(:user)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-      party_1 = create(:party)
-      party_2 = create(:party)
-      party_3 = create(:party)
-      party_4 = create(:party)
+      VCR.use_cassette("create_6_parties") do
+        party_1 = create(:party, host_id: user.id)
+        party_2 = create(:party)
+        party_3 = create(:party)
+        party_4 = create(:party)
+        party_5 = create(:party, host_id: user.id)
+        party_6 = create(:party)
 
-      require "pry"; binding.pry
+        party_2.users << user
+        party_4.users << user
+        party_6.users << user
+
+        visit '/dashboard'
+
+        within('.hosted') do
+          expect(page).to have_content(party_1.name)
+          expect(page).to have_content(party_1.date)
+          
+        end
+      end
     end
   end
 end
