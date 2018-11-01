447e4deab822e7a83eb1d64ee8d94ae7d876b22d
Kathleen Yruegas <kathleenecn@gmail.com>
2018-11-01 14:13:05 -0600

    first attempt at attending method

--- app/facades/user_party_facade.rb
+++ app/facades/user_party_facade.rb
@@ -6,4 +6,9 @@ class UserPartyFacade
   def hosting
     Party.where(host_id: @user_id)
   end
+
+  def attending
+    party_ids = UserParty.where(user_id: @user_id).pluck(:party_id)
+    Party.where(id: party_ids)
+  end
 end
