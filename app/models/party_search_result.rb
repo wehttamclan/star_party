42f184664da3a953cf09e3b277e48b7037d91cce
bghalami <benjaminghalami@gmail.com>
2018-11-01 17:40:22 -0600

    made party search look ONLY into the future, like that's so raven

--- app/models/party_search_result.rb
+++ app/models/party_search_result.rb
@@ -6,7 +6,7 @@ class PartySearchResult
   def parties
     @parties ||= (Party.where('date >= ?', Date.today).near(@zip_code, 15)).to_a
 
-    exact_zip = (Party.where(zip_code: @zip_code)).to_a
+    exact_zip = (Party.where('zip_code = ? AND date >= ?', @zip_code, Date.today)).to_a
     exact_zip.each do |party|
       @parties << party
     end
