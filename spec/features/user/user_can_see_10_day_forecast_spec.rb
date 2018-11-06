2d9d190c5b975aaead5a1ebe28d7c467686d0e36
Kathleen Yruegas <kathleenecn@gmail.com>
2018-11-06 14:33:38 -0700

    Adds sad path test for viewing forecast for invalid zip

--- spec/features/user/user_can_see_10_day_forecast_spec.rb
+++ spec/features/user/user_can_see_10_day_forecast_spec.rb
@@ -2,11 +2,6 @@ require "rails_helper"
 
 feature "As an authenticated user" do
   scenario "I can view ten day forecast with valid zip code" do
-    # work in authentication once that's built in
-    # visit '/'
-
-    # fill_in :zip_code, with: "80203"
-    # expect(current_page).to eq('/forecast')
     VCR.use_cassette("visit forecast path as user") do
       visit forecast_path(zip_code: "80203")
     end
@@ -21,5 +16,23 @@ feature "As an authenticated user" do
       expect(page).to have_css('.moon-name')
     end
     expect(page).to have_css('.select')
+  end
+
+  scenario "invalid zip prompts me to try again" do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    VCR.use_cassette("home page") do
+      visit '/'
+    end
+
+    invalid_zip = 87822
+    fill_in :zip_code, with: invalid_zip
+    VCR.use_cassette("click plan") do
+      find(".plan-party", visible: false).click
+    end
+
+    expect(current_path).to eq('/')
+    expect(page).to have_content("Sorry, no data for that zip code.")
   end
 end
