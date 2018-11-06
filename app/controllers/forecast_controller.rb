14f5184711a44083ff62a3b5fce84c52bd0d15a8
Kathleen Yruegas <kathleenecn@gmail.com>
2018-11-06 14:48:35 -0700

    refactors controller

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -1,11 +1,17 @@
 class ForecastController < ApplicationController
   def index
     @zip_code = params[:zip_code]
-    if (@zip_code.to_lat && @zip_code.to_lon)
+    if valid_zip?
       @forecast_results = ForecastFacade.new(@zip_code)
     else
       flash[:alert] = "Sorry, no data for #{@zip_code}. Please try again."
       redirect_to '/'
     end 
+  end
+
+  private
+
+  def valid_zip?
+    @zip_code.to_lat && @zip_code.to_lon
   end
 end
