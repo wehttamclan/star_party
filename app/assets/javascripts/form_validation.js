1893b34e8a4de946a3a8b01007e915450b05e224
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-11-07 11:33:00 -0700

    Deletes zipcode jquery spec.

--- app/assets/javascripts/form_validation.js
+++ app/assets/javascripts/form_validation.js
@@ -8,10 +8,4 @@ $(document).ready(function () {
       $('#party_title').css("border", '2px solid black'); 
     }
   });
-  $('#party_zip_code').ready(function () {
-    if (!this.value) {
-      $('#party_zip_code').css("border", '2px solid red');
-      $('#party_zip_code').attr("placeholder", "Zip code (required field)");
-    }
-  })
 });
